889a5239b3d7364213653c592d7ac3412992bb61
Antonio Goncalves <antonio.goncalves@gmail.com>
2015-11-05 12:08:32 +0100

    Adding Tests

---
+++ code/expenses/expenses-core/src/test/java/org/expenses/core/service/CurrencyServiceTest.java
@@ -0,0 +1,12 @@
+package org.expenses.core.service;
+
+import org.junit.Test;
+
+public class CurrencyServiceTest
+{
+
+   @Test
+   public void shoudlCheckChange()
+   {
+   }
+}
---
+++ code/expenses/expenses-core/src/test/java/org/expenses/core/service/RateServiceMock.java
@@ -0,0 +1,10 @@
+package org.expenses.core.service;
+
+public class RateServiceMock
+{
+
+   public double rate()
+   {
+      return 1.23;
+   }
+}
