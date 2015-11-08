a82b26112bc1128631c988404c0eea79e9a5c115
Antonio Goncalves <antonio.goncalves@gmail.com>
2015-11-08 10:57:16 +0100

    Changing tests

--- code/expenses/expenses-core/src/test/java/org/expenses/core/service/CurrencyServiceTest.java
+++ code/expenses/expenses-core/src/test/java/org/expenses/core/service/CurrencyServiceTest.java
@@ -4,7 +4,6 @@ import org.expenses.core.model.Currency;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.Asset;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Assert;
@@ -13,27 +12,21 @@ import org.junit.runner.RunWith;
 
 import javax.inject.Inject;
 
-import static org.junit.Assert.*;
-
-/**
- * Created by antoine on 06/11/2015.
- */
 @RunWith(Arquillian.class)
 public class CurrencyServiceTest {
+
     @Deployment
     public static JavaArchive createDeployment() {
         return ShrinkWrap.create(JavaArchive.class)
-                .addPackages(true,"org.expenses.core")
+                .addPackages(true, "org.expenses.core")
                 .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
     }
 
     @Inject
     CurrencyService currencyService;
 
-
     @Test
     public void testChangeRate() throws Exception {
-        Assert.assertEquals(1.23,currencyService.changeRate(Currency.EURO),0);
-
+        Assert.assertEquals(1.23, currencyService.changeRate(Currency.EURO), 0);
     }
 }
